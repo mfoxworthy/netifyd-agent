49bfa8c17fd252a7aca8ec82598e5b234a656842
Darryl Sokoloski <darryl@sokoloski.ca>
2021-10-14 12:27:02 -0400

    Updated to latest commits.

---
+++ util/valgrind.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=/home/dsokoloski/netify-daemon/src/.libs
+
+exec valgrind --leak-check=full --log-file=/tmp/netifyd-vg-%p.log ./src/.libs/netifyd -d -c ./src/netifyd.conf -t --thread-detection-cores 1 -I lo,./tests/pcap/engeem/BearerS1U-20200416185559.PCAP
