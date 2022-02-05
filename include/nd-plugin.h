d346f43af79dc6219ec5927f8642f3398a0fffd1
Darryl Sokoloski <darryl@sokoloski.ca>
2022-02-05 08:50:28 -0500

    Updated plugin version.

--- include/nd-plugin.h
+++ include/nd-plugin.h
@@ -17,7 +17,7 @@
 #ifndef _ND_PLUGIN_H
 #define _ND_PLUGIN_H
 
-#define _ND_PLUGIN_VER  0x20211202
+#define _ND_PLUGIN_VER  0x20220205
 
 #define ndStartDetectionThreads() kill(getpid(), SIGUSR1)
 #define ndStopDetectionThreads()  kill(getpid(), SIGUSR2)
