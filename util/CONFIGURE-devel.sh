9abae542474451f19f2f5a9d82f78ead8b119fcc
Darryl Sokoloski <darryl@sokoloski.ca>
2022-05-26 22:39:14 -0400

    Renamed.

--- util/CONFIGURE-devel.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-exec ./configure --build=x86_64-redhat-linux-gnu --host=x86_64-redhat-linux-gnu --program-prefix= --prefix=/usr --exec-prefix=/usr --bindir=/usr/bin --sbindir=/usr/sbin --sysconfdir=/etc --datadir=/usr/share --includedir=/usr/include --libdir=/usr/lib --libexecdir=/usr/libexec --localstatedir=/var --sharedstatedir=/var/lib --mandir=/usr/share/man --infodir=/usr/share/info --enable-conntrack --enable-inotify --enable-netlink --enable-plugins --enable-libtcmalloc --disable-jemalloc build_alias=x86_64-redhat-linux-gnu host_alias=x86_64-redhat-linux-gnu
