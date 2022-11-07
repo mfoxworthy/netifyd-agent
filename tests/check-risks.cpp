f91547e4ef97ef6d1ebcf8608c49a14922351f23
Darryl Sokoloski <darryl@sokoloski.ca>
2022-11-07 11:10:30 -0500

    Added test for new/missing risks.

--- tests/check-risks.cpp
+++ tests/check-risks.cpp
@@ -72,35 +72,34 @@ int main(int argc, char *argv[])
     int rc = 0;
     char proto_name[64];
 
-    cout << "Testing Netify Agent Protocols..." << endl;
+    cout << "Testing Netify Agent Risks..." << endl;
 
     ndpi_global_init();
     ndpi_detection_module_struct *ndpi = nd_ndpi_init();
 
     ndpi_protocol_id_t ndpi_protos;
-    cout << endl << "nDPI protocol count: " <<
-        NDPI_LAST_IMPLEMENTED_PROTOCOL << endl;
-    cout << "Netify Agent protocol count: " <<
-        nd_ndpi_protos.size() << endl << endl;
+    cout << endl << "nDPI risks count: " <<
+        NDPI_MAX_RISK << endl;
+    cout << "Netify Agent risks count: " <<
+        ND_RISK_MAX << endl << endl;
 
     for (uint16_t id = 0;
-        id < NDPI_LAST_IMPLEMENTED_PROTOCOL; id++) {
+        id < NDPI_MAX_RISK; id++) {
 
-        auto it = nd_ndpi_protos.find(id);
-        if (find(
-            nd_ndpi_disabled.begin(),
-            nd_ndpi_disabled.end(), id) != nd_ndpi_disabled.end()
-        ) continue;
+        auto it = nd_ndpi_risks.find(id);
+        if (it != nd_ndpi_risks.end()) continue;
 
-        if (it != nd_ndpi_protos.end()) continue;
+        ndpi_risk_enum rid = (ndpi_risk_enum)id;
 
-        ndpi_protocol proto = { id, 0 };
-        ndpi_protocol2name(
-            ndpi, proto, proto_name, sizeof(proto_name)
-        );
+        ndpi_risk_info const * const risk_info = ndpi_risk2severity(rid);
+        if(risk_info == NULL) {
+            rc = 1;
+            cout << "ID# " << id << " (ndpi_risk2severity: UNKNOWN/ERROR)" << endl;
+            continue;
+        }
 
         rc = 1;
-        cout << "ID# " << id << " (" << proto_name << ")" << endl;
+        cout << "ID# " << id << " (" << ndpi_risk2str(risk_info->risk) << ")" << endl;
     }
 
     if (rc != 0) cout << endl;
